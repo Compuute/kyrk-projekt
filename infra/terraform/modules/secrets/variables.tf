6e72531e94dca36b3ec9457094650d2f1b90ddc2
Compuute <160307775+Compuute@users.noreply.github.com>
2026-06-07 08:18:49 +0000

    fix: add missing accessors variable to secrets Terraform module

--- infra/terraform/modules/secrets/variables.tf
+++ infra/terraform/modules/secrets/variables.tf
@@ -1,2 +1,9 @@
 variable "project_id" { type = string }
 variable "names" { type = list(string) }
+variable "accessors" {
+  type = list(object({
+    secret = string
+    member = string
+  }))
+  default = []
+}
